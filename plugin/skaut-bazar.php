4e109d0ada3819238d0803188c932d8d6985fa66
Michal Janata <8988898+kalich5@users.noreply.github.com>
2021-08-15 17:14:19 +0200

    Update skaut-bazar.php

--- plugin/skaut-bazar.php
+++ plugin/skaut-bazar.php
@@ -4,7 +4,7 @@
  * Plugin name: Skaut bazar
  * Plugin URI: https://github.com/skaut/skaut-bazar
  * Description: Bazar for Scout troops
- * Version: 1.3.2
+ * Version: 1.3.3
  * Author: Junák - český skaut
  * Author URI: https://github.com/skaut
  * Text Domain: skaut-bazar
