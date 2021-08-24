edda9af5e35502cd048b313e28d051c4717752cb
Marek Dědič <developer@dedic.eu>
2021-08-24 22:35:04 +0200

    Auto-fixed phpcs issues

--- src/uninstall.php
+++ src/uninstall.php
@@ -1,9 +1,9 @@
-
 <?php
 
-	if ( !defined( 'WP_UNINSTALL_PLUGIN' ) ) 
-    	exit();
+if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
+	exit();
+}
 
-    //delete_option( 'skautbazar_option' );
+	// delete_option( 'skautbazar_option' );
 
-    
+
